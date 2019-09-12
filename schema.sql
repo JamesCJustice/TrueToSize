1e720c89f848d9bd9dd3eab00122330594a86b6b
blukat <blukatstudios@gmail.com>
2019-09-11 21:38:11 -0400

    Initial commit

---
+++ schema.sql
@@ -0,0 +1,22 @@
+
+CREATE TABLE score_submissions (
+   id SERIAL PRIMARY KEY,
+   submitter VARCHAR(255) NOT NULL,
+   shoe_type VARCHAR(255) NOT NULL,
+   score SMALLINT NOT NULL,
+   created TIMESTAMP DEFAULT now()
+);
+CREATE INDEX submitter_index
+ON score_submissions (submitter);
+CREATE INDEX shoe_type_index
+ON score_submissions (shoe_type);
+
+CREATE TABLE aggregate_scores (
+  shoe_type VARCHAR(255) PRIMARY KEY,
+  aggregate_score FLOAT(1) NOT NULL,
+  dirty BOOLEAN DEFAULT true  
+);
+
+
+DROP TABLE score_submissions;
+DROP TABLE aggregate_scores;
