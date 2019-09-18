46dce413ff96df56bc18046a47457b3bc05e104e
blukat <blukatstudios@gmail.com>
2019-09-17 21:52:37 -0400

    feat(test): Check whether db is up.

--- tests/test.js
+++ tests/test.js
@@ -9,23 +9,23 @@ const Client = require('pg').Client;
 
 describe('install and uninstall', function() {
 
-  it('should not violate laws of equality', function(done) {
-    expect('true').to.equal('true');
-    done();
-  });
-
   it('should start the postgres server', async function() {
-    this.timeout(20000); // is this necessary? FIXME
-    const client = new Client();
+    //this.timeout(20000); // is this necessary? FIXME
+    const client = new Client({ // Exclude database
+      user: process.env.PGUSER,
+      host: process.env.PGHOST,
+      port: process.env.PGPORT
+    });
     try {
         await client.connect();
         console.log("Here");
     } catch (e) {
         assert.fail("Could not connect: ${e}");
     }
-
     const { rows } = await client.query("SELECT 'Hello, world!' as message");
     expect(rows[0].message).to.match(/Hello/);
     await client.end();
   });
+
+  it('should create the database');
 });
