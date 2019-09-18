1974d9cf94f0602cc6f27a9ff14338328c197af4
blukat <blukatstudios@gmail.com>
2019-09-17 22:13:19 -0400

    feature(test): Added failing install test for each table.

--- tests/test.js
+++ tests/test.js
@@ -10,22 +10,51 @@ const Client = require('pg').Client;
 describe('install and uninstall', function() {
 
   it('should start the postgres server', async function() {
-    //this.timeout(20000); // is this necessary? FIXME
-    const client = new Client({ // Exclude database
+    const client = new Client({ // Exclude database env variable to select default database
       user: process.env.PGUSER,
       host: process.env.PGHOST,
       port: process.env.PGPORT
     });
+
     try {
         await client.connect();
-        console.log("Here");
     } catch (e) {
         assert.fail("Could not connect: ${e}");
     }
+    
     const { rows } = await client.query("SELECT 'Hello, world!' as message");
     expect(rows[0].message).to.match(/Hello/);
     await client.end();
   });
 
-  it('should create the database');
+  it('should create the database tables', async function() {
+    const client = new Client();
+
+    try {
+        await client.connect();
+    } catch (e) {
+        assert.fail("Could not connect: ${e}");
+    }
+
+    var sql = "SELECT EXISTS (" +
+              " SELECT 1" +
+              " FROM   information_schema.tables " +
+              " WHERE  table_schema = 'TrueToSize'" +
+              " AND    table_name = 'score_submissions'" +
+              ");";
+    var { rows } = await client.query(sql);
+
+    expect(rows[0].exists).to.equal(true);
+
+    var sql = "SELECT EXISTS (" +
+              " SELECT 1" +
+              " FROM   information_schema.tables " +
+              " WHERE  table_schema = 'TrueToSize'" +
+              " AND    table_name = 'aggregate_scores'" +
+              ");";
+    var { rows } = await client.query(sql);
+
+    expect(rows[0].exists).to.equal(true);
+
+  });
 });
