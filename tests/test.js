0d20755d8b7e24bb358d74409ed511103d272e40
blukat <blukatstudios@gmail.com>
2019-09-17 21:48:31 -0400

    feat(test): Check whether db is up.

--- tests/test.js
+++ tests/test.js
@@ -1,14 +1,31 @@
 const expect  = require('chai').expect;
 const request = require('request');
-var describe = require('mocha').describe;
-var before = require('mocha').before;
-var it = require('mocha').it;
-var assert = require('assert');
+const describe = require('mocha').describe;
+const before = require('mocha').before;
+const it = require('mocha').it;
+const assert = require('assert');
+const Client = require('pg').Client;
+
 
 describe('install and uninstall', function() {
 
   it('should not violate laws of equality', function(done) {
     expect('true').to.equal('true');
     done();
+  });
+
+  it('should start the postgres server', async function() {
+    this.timeout(20000); // is this necessary? FIXME
+    const client = new Client();
+    try {
+        await client.connect();
+        console.log("Here");
+    } catch (e) {
+        assert.fail("Could not connect: ${e}");
+    }
+
+    const { rows } = await client.query("SELECT 'Hello, world!' as message");
+    expect(rows[0].message).to.match(/Hello/);
+    await client.end();
   });
 });
